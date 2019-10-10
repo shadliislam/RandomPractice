1cea374e678ac0c6e8ca522d82a357379f905667
shadliislam <shadli.islam@gmail.com>
2019-10-10 14:31:39 -0400

    INITIAL

---
+++ src/main/java/PrintReverseTriangle.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class PrintReverseTriangle {
+    public static void printTriangle(int number){
+        for (int row=0;row<number;row++){
+            int num=1;
+            for (int col=1;col<=number-row;col++) {
+                System.out.print(num+" ");
+                num++;
+            }
+            System.out.println("");
+            for (int h=0;h<=row;h++) {
+                if(row==number-1)
+                    break;
+                else System.out.print("-");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int number=scanner.nextInt();
+        printTriangle(number);
+    }
+}
